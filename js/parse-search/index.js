6844199d0b6ed800a81304928438385fe2b555d5
Moren YANG <morenyang88@gmail.com>
2018-06-05 14:52:02 +0800

    update parse-search

--- js/parse-search/index.js
+++ js/parse-search/index.js
@@ -1,9 +1,15 @@
+/**
+ * paser location.search to map
+ * @param {String} search
+ * @returns {Object} map
+ */
 function parseSearch(search) {
   search = search.replace(/\?/, '');
   var obj = {};
   var querys = search.split('&');
   querys.forEach(item => {
     var [key, value] = item.split('=');
+    key = decodeURIComponent(key);
     value = decodeURIComponent(value);
     if (obj.hasOwnProperty(key)) {
       if (Array.isArray(obj[key])) {
@@ -12,7 +18,7 @@ function parseSearch(search) {
         obj[key] = [obj[key], value];
       }
     } else {
-      obj[key] = value;
+      obj[key] = value || '';
     }
   });
   return obj;
@@ -20,7 +26,11 @@ function parseSearch(search) {
 
 // test
 // var qs = require('querystring');
-// var str = qs.stringify({ a: '& 1=&&%%\\||', b: ['hello', 1, 2, 3, [1, 2]], c: [1, 2], d: { a: '!22' } });
+// var str = qs.stringify({ a: '& 1=&&%%\\||', b: ['hello', 1, 2, 3, [1, 2]], c: [1, 2], d: { a: '!22' }, 你好: "世界" });
 // console.log(str);
 // console.log(JSON.stringify(qs.parse(str)));
 // console.log(JSON.stringify(parseSearch(str)));
+
+// str1 = "=&="
+// console.log(JSON.stringify(qs.parse(str1)));
+// console.log(JSON.stringify(parseSearch(str1)));
